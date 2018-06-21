57352de98e61718ce92c59c831ed525a2cf4df3e
bokken12 <stopwatchj@gmail.com>
2018-06-21 10:07:41 -0700

    Added Grading View
    Highlights the grading button of students the instructor has yet to grade.

--- script.js
+++ script.js
@@ -12,24 +12,40 @@ var HttpClient = function() {
     }
 }
 
-var client = new HttpClient();
+if(document.URL.startsWith('https://lms.alphastar.academy/course/view.php')){
+  console.log("LMSGrader assignment view");
+  var client = new HttpClient();
 
-var weeks = document.body.lastElementChild.children[1].firstElementChild.firstElementChild.firstElementChild.children[1].children[2].children[2];
-for (var i = 0; i < weeks.children.length; i++) {
-  var activities = weeks.children[i].children[3].children[3].children;
-  for (var j = 0; j < activities.length; j++) {
-    if(activities[j].className.startsWith("activity assign modtype_assign")){
-      let activity = activities[j].children[0].children[0].children[1].children[0].children[0];
-      let link = activity.href;
-      client.get(link, function(response) {
-        let index = response.indexOf('<td class="cell c0" style="">Needs grading</td>\n<td class="cell c1 lastcol" style="">');
-        response = response.substring(index+85);
-        let index2 = response.indexOf("</td>");
-        response = response.substring(0, index2);
-        if(response != 0){
-          activity.innerHTML = '<style> strong{ color: red; }</style><strong>(' + response + ' to grade)</strong> ' + activity.innerHTML;
-        }
-      })
+  var weeks = document.body.lastElementChild.children[1].firstElementChild.firstElementChild.firstElementChild.children[1].children[2].children[2];
+  for (var i = 0; i < weeks.children.length; i++) {
+    var activities = weeks.children[i].children[3].children[3].children;
+    for (var j = 0; j < activities.length; j++) {
+      if(activities[j].className.startsWith('activity assign modtype_assign')){
+        let activity = activities[j].children[0].children[0].children[1].children[0].children[0];
+        let link = activity.href;
+        client.get(link, function(response) {
+          let index = response.indexOf('<td class="cell c0" style="">Needs grading</td>\n<td class="cell c1 lastcol" style="">');
+          response = response.substring(index+85);
+          let index2 = response.indexOf("</td>");
+          response = response.substring(0, index2);
+          if(response != 0){
+            activity.innerHTML = '<style> strong{ color: red; }</style><strong>(' + response + ' to grade)</strong> ' + activity.innerHTML;
+          }
+        })
+      }
+    }
+  }
+} else if (document.URL.startsWith('https://lms.alphastar.academy/mod/assign/view.php?id=')){
+  console.log("LMSGrader grading view");
+  var users = document.getElementsByClassName("box boxaligncenter gradingtable")[0].children[3].children[0].children[1];
+  for (var i = 0; i < users.children.length; i++) {
+    let child = users.children[i];
+    let dateSubmitted = child.children[8].textContent;
+    let dateGraded = child.children[11].textContent;
+    if(dateSubmitted !== '-'){
+      if(dateGraded === '-' || new Date(dateSubmitted) >= new Date(dateGraded)){
+        child.children[6].children[0].className = 'btn btn-warning';
+      }
     }
   }
 }
